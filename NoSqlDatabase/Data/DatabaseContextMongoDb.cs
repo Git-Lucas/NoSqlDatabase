5650d81788573f7b73a5869256a17773884c568f
Git-Lucas <lucas.oliver_2000@hotmail.com>
2024-05-02 10:34:11 -0300

    [NSD] - feat: Criada classe que conecta ao MongoDb e possui os métodos para manipulação dos bancos de dados, coleções e documentos

---
+++ NoSqlDatabase/Data/DatabaseContextMongoDb.cs
@@ -0,0 +1,90 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace NoSqlDatabase.Data;
+
+public class DatabaseContextMongoDb(IConfiguration configuration)
+{
+    private readonly MongoClient _client = new(configuration.GetConnectionString("DefaultConnection"));
+    private Dictionary<string, List<string>>? _databasesAndCollections;
+
+    public async Task<Dictionary<string, List<string>>> GetDatabaseAndCollections()
+    {
+        if (_databasesAndCollections is not null)
+        {
+            return _databasesAndCollections;
+        }
+
+        _databasesAndCollections = [];
+
+        IAsyncCursor<string> databasesResult = _client.ListDatabaseNames();
+
+        await databasesResult.ForEachAsync(async databaseName =>
+        {
+            List<string> collectionNames = [];
+            IMongoDatabase database = _client.GetDatabase(databaseName);
+            IAsyncCursor<string> collectionNamesResult = database.ListCollectionNames();
+            await collectionNamesResult.ForEachAsync(collectionName =>
+            {
+                collectionNames.Add(collectionName);
+            });
+            _databasesAndCollections.Add(databaseName, collectionNames);
+        });
+
+        return _databasesAndCollections;
+    }
+
+    public async Task<BsonDocument> GetDocument(string databaseName, string collectionName, int index)
+    {
+        IMongoCollection<BsonDocument> collection = GetCollection(databaseName, collectionName);
+
+        BsonDocument document = [];
+
+        await collection
+            .Find(doc => true)
+            .Skip(index)
+            .Limit(1)
+            .ForEachAsync(doc => document = doc);
+
+        return document;
+    }
+
+    public async Task<long> GetCollectionCount(string databaseName, string collectionName)
+    {
+        IMongoCollection<BsonDocument> collection = GetCollection(databaseName, collectionName);
+        return await collection.EstimatedDocumentCountAsync();
+    }
+
+    public async Task<UpdateResult> CreateOrUpdateField(string databaseName, string collectionName, string id, string fieldName, string value)
+    {
+        IMongoCollection<BsonDocument> collection = GetCollection(databaseName, collectionName);
+        UpdateDefinition<BsonDocument> update = Builders<BsonDocument>.Update.Set(fieldName, new BsonString(value));
+        
+        return await collection.UpdateOneAsync(CreateIdFilter(id), update);
+    }
+
+    public async Task<DeleteResult> DeleteDocument(string databaseName, string collectionName, string id)
+    {
+        IMongoCollection<BsonDocument> collection = GetCollection(databaseName, collectionName);
+
+        return await collection.DeleteOneAsync(CreateIdFilter(id));
+    }
+
+    public async Task CreateDocument(string databaseName, string collectionName)
+    {
+        IMongoCollection<BsonDocument> collection = GetCollection(databaseName, collectionName);
+
+        await collection.InsertOneAsync([]);
+    }
+
+    private IMongoCollection<BsonDocument> GetCollection(string databaseName, string collectionName)
+    {
+        IMongoDatabase db = _client.GetDatabase(databaseName);
+        return db.GetCollection<BsonDocument>(collectionName);
+    }
+
+    private static BsonDocument CreateIdFilter(string id)
+    {
+        return new BsonDocument("_id", new BsonObjectId(new ObjectId(id)));
+    }
+}
